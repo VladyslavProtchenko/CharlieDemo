a2d296b116d50acd9adc22a697d08c69677691be
VladyslavProtchenko <vladyslav25cm@gmail.com>
2026-01-04 21:39:11 +0200

    added configurations for photos from supabase

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     unoptimized: process.env.NODE_ENV === 'development',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
   },
 };
 
